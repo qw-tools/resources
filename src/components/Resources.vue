7fe83dbd98fc3790feb09ec53d5c31a1789da89b
Viktor Persson <viktor.persson@arcsin.se>
2023-02-17 23:57:30 +0100

    Update resource list.

--- src/components/Resources.vue
+++ src/components/Resources.vue
@@ -32,20 +32,22 @@ watch(query, onQueryChange, { immediate: true });
     <div class="container flex items-center h-16">
       <h1 class="font-bold text-xl mr-6">Resources</h1>
       <div class="grow max-w-xs">
-        <input type="search" class="p-2 bg-gray-100 w-full rounded-md shadow border border-gray-300 text-sm focus:bg-sky-50 focus:border-sky-600 focus:outline-0" placeholder="filter" v-model="query"
+        <input type="search"
+               class="p-2 bg-gray-100 w-full rounded-md shadow border border-gray-300 text-sm focus:bg-sky-50 focus:border-sky-600 focus:outline-0"
+               placeholder="filter" v-model="query"
                autofocus />
       </div>
     </div>
   </div>
 
   <div class="py-4">
-    <div class="container space-y-4">
+    <div class="container space-y-6">
 
       <div v-for="c in collections">
-        <div class="font-bold text-xl py-2 pl-4 border-l-4 border-l-purple-400 bg-purple-400/10 flex">
+        <div class="font-bold text-xl py-2 pl-4 border-l-4 border-l-purple-400 bg-purple-100">
           {{ c.category.title }} ({{ c.items.length }})
         </div>
-        <div class="py-2 divide-y divide-white/10 md:divide-y-0 md:grid md:gap-x-14 md:grid-cols-2 xl:grid-cols-4">
+        <div class="py-2 divide-y md:divide-y-0 md:grid md:gap-x-14 md:grid-cols-2 xl:grid-cols-4">
           <ItemLink v-for="i in c.items" :key="i.title" :item="i" />
         </div>
       </div>
