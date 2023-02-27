55968881c19c332de639911d959f36ab9a965057
Viktor Persson <viktor.persson@arcsin.se>
2023-02-27 21:25:41 +0100

    Update logotypes.

--- src/data/legacy.ts
+++ src/data/legacy.ts
@@ -12,12 +12,14 @@ function toItem(item: Partial<Item>): Item {
 export const legacy: Item[] = [
   {
     title: "EQL",
+    logo: "eql.png",
     slogan: "European Quake League",
     url: "https://eql.quakeworld.nu/",
     related: ["https://www.quakeworld.nu/wiki/European_Quake_League"],
   },
   {
     title: "NQR",
+    logo: "nqr.png",
     slogan: "Nations Quake Rank",
     url: "https://nqr.quakeworld.nu/",
     related: ["https://www.quakeworld.nu/wiki/Nations_Quake_Rank"],
